d0131f2364313aa4300b721f1b1e92fc36ba8770
João Henrique <fontnelle@hotmail.com>
2021-10-13 22:42:57 -0300

    Alterando texto

--- assets/js/biometria.js
+++ assets/js/biometria.js
@@ -8,14 +8,36 @@ var perguntas = {
 
 	pergunta_titulo_1: {
 		sigla: 'pergunta_titulo_1',
-		titulo: 'Quem precisa cadastrar?',
-		resposta: '<p>Todos os colaboradores devem inserir certificações, exames e provas, independente da área ou tema.</p><p>Somos uma empresa de TI, logo teremos muitas certificações/exames nesta área, mas também contamos com profissionais formados nas mais diversas áreas que também são importantes e devem ser inseridas no módulo de certificações e exames.</p>'
+		titulo: 'Documentação Necessária para Efetivação',
+		resposta: '<p>Para efetivação do registro, você deverá preencher essa Ficha de Cadastro e anexar cópias digitais dos documentos abaixo relacionados, além de entregar o original do <a href="#Atestado">Atestado Admissional</a> (e laudo PCD, se for o caso) na sede da Hepta*.</p>'+
+		'<li>Documento oficial de identidade (Carteira de identidade ou CNH)</li>'+
+		'<li>CPF</li>'+
+		'<li>Certidão de casamento ou união estável (se for o caso)</li>'+
+		'<li>Cartão PIS ou NIT (se tiver)</li>'+
+		'<li>Certificado de reservista ou Dispensa do Serviço Militar (se for o caso)</li>'+
+		'<li>Título de Eleitor (ou certidão de quitação eleitoral emitida no site do TSE)</li>'+
+		'<li>Comprovante de residência</li>'+
+		'<li>Certidão de nascimento dos filhos (se for o caso)</li>'+
+		'<li>Carteira de vacinação dos filhos menores de 7 anos (se for o caso)</li>'+
+		'<li>Declaração semestral de frequência na escola dos filhos, a partir dos 7 anos (se for o caso)</li>'+
+		'<li>Em caso PCD, laudo com enquadramento (anexar cópia digital na área de documentos desse formulário e entregar original na empresa)</li>'+
+		'<li><a href="#AtestadoMedico">Atestado admissional</a> (anexar cópia digital na área de documentos desse formulário e entregar/enviar original)</li>'+
+		'<p>É importante que você já organize os documentos a serem enviados e agende a realização do exame admissional. O prazo para preenchimento e envio da Ficha de Cadastro é de 48 horas.</p>'+
+		'<hr>'+
+		'<span>* Se você é de outra localidade, pode enviar o Atestado e Laudo pelos Correios e será reembolsado.</span>'
 	},
 	pergunta_titulo_2: {
 		sigla: 'pergunta_titulo_2',
-		titulo: 'O que deve ser cadastrado?',
-		resposta: '<p>Toda e qualquer certificação e exames.</p> <p> Todos os exames/provas e certificações dos diferentes fornecedores (AWS, Microsoft, Citrix, HDI, etc) devem ser inseridos no sistema. Lembre-se que algumas provas são utilizadas para mais de uma certificação, por isso, é muito importante que o colaborador cadastre todos os exames/provas que fez e, também as certificações.</p> <p>Não se preocupe se o exame/prova ou a certificação que você fez não estiver na lista. Clique aqui para indicar o cadastro e a Academia Hepta irá te informar quando estiver disponível.</p>'+
-		'<span class="tags">Tags: título anulado, título revogado, título inválido, título suspenso, título com problema.</span>'
+		titulo: 'Atestado Médico Admissional',
+		resposta: '<p>No Distrito Federal e São Paulo, os colaboradores devem agendar horário de atendimento na empresa Brasilmed e se apresentar na data agendada portando a Guia de Atendimento. Como a Hepta possui convênio com a Brasilmed, não será cobrado de você nenhum valor pelos serviços de exames médicos. </p>'+
+		'<p>Os colaboradores em outras cidades deverão procurar uma clínica especializada e realizar o exame admissional por conta própria. A nota fiscal deste exame deverá ser encaminhada digitalizada para o e-mail  departamentopessoal@hepta.com.br para que seja feito o reembolso imediato.</p>'+
+		'<h4>IMPORTANTE: O ATESTADO MÉDICO ADMISSIONAL é documento obrigatório, não sendo possível proceder com a contratação sem a sua apresentação.</h4>'+
+		'<h3>Agendando o seu exame com a Brasilmed</h3>'+
+		'<p>Entre em contato com a Brasilmed para agendar o exame na unidade de sua preferência.</p>'+
+		'<li>São Paulo: (11) 3042-6141</li>'+
+		'<li>Brasília: (61) 3574-7600</li>'+
+		'<li>Site da Brasilmed: <a href="http://www.brasilmed.com.br" alt="Acessar site">www.brasilmed.com.br</a></li>'+
+		'<li>SAC: <a href="mailto:sac@brasilmed.com.br" alt="e-mail Brasilmed">sac@brasilmed.com.br</a></li>'
 	},
 	pergunta_titulo_3: {
 		sigla: 'pergunta_titulo_3',
@@ -40,6 +62,15 @@ var perguntas = {
 	pergunta_titulo_7: {
 		sigla: 'pergunta_titulo_7',
 		titulo: 'Conhecendo o formulário',
+		resposta: '<p>Em certificação/Exame, ao começar a digitar pelo termo da certificação ou exame irá aparecer uma lista para que você encontre mais rápido o que procura.</p>'+
+		'<p><img src="assets/img/img-1.png" alt="formulário do sistema"></p>'+
+		'<p>Se você não encontrar sua prova ou certificação na lista, basta clicar no link disponível na parte inferior do cadastro e preencher o formulário com os dados solicitados. Em <strong>documento, anexe o arquivo da certificação ou exame cadastrado</strong>. O sistema aceita arquivo em <strong>PDF com limite de tamanho de até 1GB</strong>.</p>'+
+		'<img src="assets/img/img-2.png" alt="imagem do anexo até  1gb">'+
+		'<p>Por fim, informe a data de emissão e data de validade do exame ou certificação. Antes de clicar em ENVIAR, verifique se todos os dados estão corretos. Se mudar de ideia e quiser cadastrar em outro momento é só cancelar ou sair da página.</p>'
+	},
+	pergunta_titulo_8: {
+		sigla: 'pergunta_titulo_8',
+		titulo: 'vamos que vamos',
 		resposta: '<p>Em certificação/Exame, ao começar a digitar pelo termo da certificação ou exame irá aparecer uma lista para que você encontre mais rápido o que procura.</p>'+
 		'<p><img src="assets/img/img-1.png" alt="formulário do sistema"></p>'+
 		'<p>Se você não encontrar sua prova ou certificação na lista, basta clicar no link disponível na parte inferior do cadastro e preencher o formulário com os dados solicitados. Em <strong>documento, anexe o arquivo da certificação ou exame cadastrado</strong>. O sistema aceita arquivo em <strong>PDF com limite de tamanho de até 1GB</strong>.</p>'+
