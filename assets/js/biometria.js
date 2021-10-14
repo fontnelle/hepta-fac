387ef472dc56300d698ae0265fbf44bb5114e9e2
João Henrique <fontnelle@hotmail.com>
2021-10-14 12:29:56 -0300

    Ajuste mobile

--- assets/js/biometria.js
+++ assets/js/biometria.js
@@ -31,7 +31,7 @@ var perguntas = {
 		titulo: 'Atestado Médico Admissional',
 		resposta: '<p>No Distrito Federal e São Paulo, os colaboradores devem agendar horário de atendimento na empresa <a href="https://brasilmed.com.br/" target="_blank" alt="Brasilmed">Brasilmed</a> e se apresentar na data agendada portando a <a href="#" alt="Guia de Atendimento">Guia de Atendimento</a>. Como a Hepta possui convênio com a Brasilmed, não será cobrado de você nenhum valor pelos serviços de exames médicos. </p>' +
 			'<p>Os colaboradores em outras cidades deverão procurar uma clínica especializada e realizar o exame admissional por conta própria. A nota fiscal deste exame deverá ser encaminhada digitalizada para o e-mail  <a href="mailto:departamentopessoal@hepta.com.br" alt="departamentopessoal@hepta.com.br">departamentopessoal@hepta.com.br</a> para que seja feito o reembolso imediato.</p>' +
-			'<h4>IMPORTANTE: O ATESTADO MÉDICO ADMISSIONAL é documento obrigatório, não sendo possível proceder com a contratação sem a sua apresentação.</h4>' +
+			'<p><strong>IMPORTANTE: O ATESTADO MÉDICO ADMISSIONAL é documento obrigatório, não sendo possível proceder com a contratação sem a sua apresentação.</strong></p>' +
 			'<h4>Agendando o seu exame com a Brasilmed</h4>' +
 			'<p>Baixe a <a href="#GuiaBrasilmed" alt="Guia de Atendimento da Brasilmed">Guia de Atendimento da Brasilmed</a>, preencha com os seus dados, imprima e assine.</p>'+
 			'<p>Entre em contato com a Brasilmed para agendar o exame na unidade de sua preferência.</p>' +
@@ -67,7 +67,7 @@ var perguntas = {
 			'<p>A Hepta tem contrato com a Amil para fornecer Plano de Saúde Coletivo Empresarial para seus colaboradores. O benefício e seus respectivos descontos seguirão o disposto no Acordo e/ou Convenção Coletiva de Trabalho de sua localidade (cidade e/ou estado).</p>' +
 			'<p>Até 30 dias após a admissão a inclusão do plano de saúde é feita sem carência, após essa data a inclusão já é feita com carência. O plano de saúde padrão é o AMIL S450 QC (Quarto Coletivo), sendo que os percentuais de participação patronal (Hepta) e funcional (funcionário) variam de acordo com o acordo/convenção coletiva de trabalho de cada localidade. </p>' +
 			'<p>Também é oferecida ao colaborador a opção de adesão a planos superiores, sendo que nesse caso irá pagar integralmente a diferença de valor entre o plano escolhido e o plano padrão AMIL S450 QC.</p>' +
-			'<p><img src="assets/img/tabela-plano-saude.png"></p>' +
+			'<p><img class="img-fluid" src="assets/img/tabela-plano-saude.png"></p>' +
 			'<p>Importante lembrar que:</p>' +
 			'<li><strong>Não mais existe carteira física para Plano de Saúde e Plano Dental</strong></li>' +
 			'<li>Por definição da Agência Nacional de Saúde (ANS), para planos coletivos empresariais são considerados como dependentes apenas o cônjuge e filhos.</li>' +
@@ -75,7 +75,7 @@ var perguntas = {
 			'<li><strong>O empregado afastado por alguma enfermidade que passar a receber Auxílio Doença do INSS continuará usufruindo do Plano de Saúde, juntamente com seus dependentes legais, sendo responsável por arcar com o pagamento de sua parte cabível (O Pagamento do plano de saúde será feito em depósito na conta da Hepta).</strong></li>' +
 			'<h4>Plano Dental</h4>' +
 			'<p>Além do plano de saúde, o colaborador Hepta também pode aderir ao plano odontológico <a href="https://www.amildental.com.br/" target="_blank" alt="Amil Dental">AMIL Dental</a>. O custo mensal deste plano é descontado integralmente no salário de acordo com a tabela abaixo:</p>' +
-			'<p><img src="assets/img/plano-odonto.png" alt="Plano Odontológico"></p>'
+			'<p><img class="img-fluid" src="assets/img/plano-odonto.png" alt="Plano Odontológico"></p>'
 	},
 	pergunta_titulo_5: {
 		sigla: 'pergunta_titulo_5',
